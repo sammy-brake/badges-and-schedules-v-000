c7cf29b6763860c2055e83b12a6c98290f32158c
Samantha brake <sammy.brake@gmail.com>
2018-08-28 14:32:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+return  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+   new_array.push(badge_maker(name))
+  end 
+  return new_array
+end 
